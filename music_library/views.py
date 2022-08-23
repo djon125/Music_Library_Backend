f2ecb50ca318766be80dacee8c474362f646ed44
djon125 <ross.dion9027@gmail.com>
2022-08-23 17:24:58 -0500

    created get all songs view function

--- music_library/views.py
+++ music_library/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
+from music_library import serializers
 
 @api_view(['GET'])
 def songs_list(request):
+    songs = Song.objects.all()
     
+    serializer = SongSerializer(songs, many=True)
     
-    return Response('ok')
+    return Response(serializer.data)
