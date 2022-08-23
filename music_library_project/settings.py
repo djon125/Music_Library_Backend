319f0d4e4b17faed218d78254eec8a7b9ef607e6
djon125 <ross.dion9027@gmail.com>
2022-08-23 18:43:32 -0500

    added cors

--- music_library_project/settings.py
+++ music_library_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'music_library'
+    'music_library',
+    'corsheader'
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL=True
 
 ROOT_URLCONF = 'music_library_project.urls'
 
