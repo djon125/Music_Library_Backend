a42f4849bcea85e84cd0a6b259872ddc60efa752
djon125 <ross.dion9027@gmail.com>
2022-08-25 16:42:29 -0500

    added search functionality on get request

--- music_library/views.py
+++ music_library/views.py
@@ -11,17 +11,28 @@ from .models import Song
 def songs_list(request):  
     
     if request.method == 'GET':
-        genre_name= request.query_params.get('genre') 
-        print(genre_name)
         songs = Song.objects.all()
+        genre_name = request.query_params.get('genre') 
+        artist_name = request.query_params.get('artist')
+        album_name = request.query_params.get('album')
+        release_date = request.query_params.get('date')
+        title_name = request.query_params.get('title')
         if genre_name:
             songs= songs.filter(genre__exact=genre_name)
         serializer = SongSerializer(songs, many=True)
-        return Response(serializer.data)
-
-        
-    
-    
+        if artist_name:
+            songs =songs.filter(artist__exact=artist_name)
+        serializer = SongSerializer(songs, many=True)
+        if album_name:
+            songs =songs.filter(album__exact=album_name)
+        serializer = SongSerializer(songs, many=True)
+        if release_date:
+            songs =songs.filter(release_date__exact=release_date)
+        serializer = SongSerializer(songs, many=True)
+        if title_name:
+            songs =songs.filter(title__exact=title_name)
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)   
     elif request.method == 'POST':
         serializer = SongSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
